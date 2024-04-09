118957fabffe3e1ba078872f393e991c60c96d16
mhusberg <m.husberg@gmail.com>
2024-04-09 22:04:39 +0200

    added some audio clips ;)

---
+++ hondle/src/helpers/PlayAudio.ts
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+interface Sound {
+    id: string;
+    src: string;
+}
+const soundList: Sound[] = [
+    {id: "big_bear", src: ""},
+]
+
+export const PlayAudio = () => {
+
+};
